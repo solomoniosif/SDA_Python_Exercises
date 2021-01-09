5aad423534e8d80fa38829e25385d40c2636443b
solomoniosif <solomoniosif@gmail.com>
2021-01-09 22:29:16 +0200

    modified 9_01_exercitiu_cezar.py

--- 9_ianuarie_2021/9_01_exercitiu_cezar.py
+++ 9_ianuarie_2021/9_01_exercitiu_cezar.py
@@ -1,43 +1,27 @@
 import random
-import math
 
-aa = random.random() * 10000
-print(aa)
-print(type(aa))
-b = str(aa)
+def multiply_by(factor):
+    number = random.random() * 10000
+    str_number = str(number)
+    result = []
+    remainder = 0
+    for digit in reversed(str_number):
+        if digit == '.':
+            result.append(digit)
+            continue
+        f = int(digit) * factor + remainder
+        result.append(str(f % 10))
+        remainder = f // 10
+    if remainder > 0:
+        result.append(str(remainder))
+    print()
+    print(number)
+    print(float(''.join(reversed(result))))
 
-print(b)
-print(type(b))
+multiply_by(2)
+multiply_by(5)
 
-
-# result = []
-# r = 0
-# for d in b[::-1]:
-#     if d == '.':
-#         result.append(d)
-#         continue
-#     e = int(d)
-#     f = e * 2 + r
-#     result.append(str(f % 10))
-#     r = f // 10
-# if r > 0:
-#     result.append(str(r))
-# print(aa)
-# print(''.join(reversed(result)))
-# print()
-# aaa = random.random() * 10000
-# b = str(aaa)
-# result = []
-# r = 0
-# for d in b[::-1]:
-#     if d == '.':
-#         result.append(d)
-#         continue
-#     e = int(d)
-#     f = e * 5 + r
-#     result.append(str(f % 10))
-#     r = f // 10
-# if r > 0:
-#     result.append(str(r))
-# print(aaa)
-# print(''.join(reversed(result)))
+########################################################################
+# 1. Am șters 'import math' pentru că modulul math nu era folosit nicăieri în cod
+# 2. Am înlocuit '[::-1]' cu 'reversed()' în prima parte a codului pentru că e mai eficient și pentru consistență cu restul codului
+# 3. Pentru a nu repeta codul am creat o funcție numită 'multiply_by' care acceptă ca parametru al doilea factor al înmulțirii
